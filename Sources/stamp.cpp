fab9714e24f984e233c6f03e827950232db6fed5
aerror2 <aerror@outlook.com>
2022-01-12 10:07:53 +0800

    add version info

--- Sources/stamp.cpp
+++ Sources/stamp.cpp
@@ -4,12 +4,12 @@
 #define STR2(s) #s
 #define DEFNUMSTR(s)  STR2(s)
 
-#define DATE_STR "29.10.2019"
-#define TIME_STR "08:15:02"
+#define DATE_STR "11.01.2022"
+#define TIME_STR "23:15:02"
 #define SUB_VERS  10
-#define SVN_VERS "1.0.3"
+#define SVN_VERS "1.0.4"
 #define MOD_VERS "FlySky-I6 Base"
-#define AUTOR    "Kotello"
+#define AUTOR    "Kotello,Aerror2"
 
 const char Stamps[] = "VERS: V" DEFNUMSTR(VERS) "." DEFNUMSTR(SUB_VERS) " -" AUTOR "\037"\
 " MOD: " MOD_VERS;
