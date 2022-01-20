4f60af09df000e991712bf4c8ee475df50ccba9a
aerror2 <aerror@outlook.com>
2022-01-20 14:16:12 +0800

    add stamp v1.0.5

--- Sources/stamp.cpp
+++ Sources/stamp.cpp
@@ -4,10 +4,10 @@
 #define STR2(s) #s
 #define DEFNUMSTR(s)  STR2(s)
 
-#define DATE_STR "11.01.2022"
+#define DATE_STR "20.01.2022"
 #define TIME_STR "23:15:02"
 #define SUB_VERS  10
-#define SVN_VERS "1.0.4"
+#define SVN_VERS "1.0.5"
 #define MOD_VERS "FlySky-I6 Base"
 #define AUTOR    "Kotello,Aerror"
 
